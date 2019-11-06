b25bc3714fea2a99d1863be7abb0cd539d3c0668
intfloatbool <intfloatbool@gmail.com>
2019-11-06 17:38:19 +0300

    CrowdSaleContract

---
+++ contracts/BorgTokenCrowdsale.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.5.8;
+
+import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
+import "@openzeppelin/contracts/crowdsale/emission/MintedCrowdsale.sol";
+
+contract BorgTokensCrowdsale is Crowdsale , MintedCrowdsale {
+    constructor(uint256 _rate, 
+                address payable _wallet, 
+                ERC20 _token) Crowdsale(_rate, _wallet, _token) public {
+
+    }
+}
