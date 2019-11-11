1a7b15cbdf700ddd0855e95190d84c5c3d566900
intfloatbool <intfloatbool@gmail.com>
2019-11-11 17:11:34 +0300

    MultiSigWalletSplitter

--- contracts/WalletSplitter.sol
+++ contracts/WalletSplitter.sol
@@ -2,10 +2,9 @@ pragma solidity 0.5.8;
 
 import "@openzeppelin/contracts/payment/PaymentSplitter.sol";
 import "@openzeppelin/contracts/access/roles/SignerRole.sol";
-import "./MultiSigWallet.sol";
 
-contract WalletSplitter is PaymentSplitter, SignerRole, MultiSigWallet {
-    constructor(address[] memory payees, uint256[] memory shares) PaymentSplitter(payees, shares) MultiSigWallet(payees, payees.length) public {
+contract WalletSplitter is PaymentSplitter, SignerRole {
+    constructor(address[] memory payees, uint256[] memory shares) PaymentSplitter(payees, shares) public {
         //payees - кошельки между которыми делятся деньги
         //payees - доли каждого кошелька (например у одного 10% у другого 90%)
 
